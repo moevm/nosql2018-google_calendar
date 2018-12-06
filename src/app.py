f08ef5704c2186f4229424b11d5a912ac430ee34
Olga <kochnevaolga74@bk.ru>
2018-12-06 12:28:24 +0300

    Write user calendar to mongodb

--- src/app.py
+++ src/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify, render_template, make_response, send_file,  url_for, flash, redirect, send_from_directory
 from flask_pymongo import PyMongo
-import os
+from parse_to_json import createjson
 from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+import json
+import dateutil.parser
+
 
 UPLOAD_FOLDER = '/tmp'
 ALLOWED_EXTENSIONS = set(['ics', 'json', 'bson'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config.from_json("config.json")
+mongo = PyMongo(app)
+
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -27,45 +35,87 @@ def main():
             filename = secure_filename(file.filename)
             #file - это то что показывала тебе
             #filename - просто имя файла.
-            return file
-            '''file_contents = file.read()
-            response = make_response(file_contents)
-            response.headers["Cache-Control"] = "must-revalidate"
-            response.headers["Pragma"] = "must-revalidate"
-            response.headers["Content-type"] = "application/ics"
-            print(file)
-            return str(response)
-
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))'''
+            username = createjson(file)
+            parse_to_mongo_user(username)
 
     return render_template('main.html')
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            # username = str(os.path.abspath(filename))
+            # parse_to_mongo_user(username)
+
 
 @app.route('/tmp/<filename>')
 def uploaded_file(filename):
     return send_file(app.config['UPLOAD_FOLDER'],
                                filename)
 
+
 @app.route('/statistics')
 def statistics():
     return render_template('statistics.html')
+
 
 @app.route('/lovelyFriend')
 def lovelyFriend():
     return render_template('lovelyFriend.html')
 
+
 @app.route('/organizedEvents')
 def solvedProblems():
     return render_template('/organizedEvents.html')
+
 
 @app.route('/employment')
 def employment():
     return render_template('/employment.html')
 
+
 @app.route('/synchronization')
 def synchronization():
     return render_template('/synchronization.html')
+
+
+def screen_5(is_year):
+    cursor = mongo.db.users.find({"email": "kochnevaolga74@gmail.com"})
+    cur_date = datetime.now()
+    if is_year:
+        cur_date = cur_date - timedelta(days=365)
+    else:
+        cur_date = cur_date - timedelta(days=30)
+
+    print(cur_date)
+
+    count = 0  # вернет мероприятия где ты организатор
+    count2 = 0  # вернет все мероприятия
+    for id in cursor:
+        user_id = id.get('_id')
+        count = mongo.db.events.find({"user_id": user_id, "organizer": "kochnevaolga74@gmail.com", "start": {'$gte': cur_date}}).count()
+        count2 = mongo.db.events.find({"user_id": user_id, "start":{'$gte': cur_date}}).count()
+
+    return [count, count2]
+
+
+def parse_to_mongo_user(user):  # закидываем юзера в бд юзеров
+    mongo.db.users.remove({})
+    mongo.db.users.insert({"email": user})
+    cursor = mongo.db.users.find({"email": user})
+    for id in cursor:
+        user_id = id.get('_id')
+        parse_to_mongo_events(user_id, user)  # вызываем функцию для закидывания мероприяти1
+
+
+def parse_to_mongo_events(user_id, user):  # закидываем его мероприятия
+    mongo.db.events.remove({})
+    json_file = open('./data/' + user + '.json').read()
+    json_file = json.loads(json_file)
+    for event in json_file:
+        dtstart = dateutil.parser.parse(event["DTSTART"]) + timedelta(hours=3) # преобразуем в человеческий формат дату плюс 3 часа потому
+        dtend = dateutil.parser.parse(event["DTEND"]) + timedelta(hours=3)
+        mongo.db.events.insert({"user_id": user_id, "summary": event["SUMMARY"],
+                                "start": dtstart, "end": dtend, "location": event["LOCATION"],
+                                "organizer": event["ORGANIZER"], "visitors": event["VISITORS"]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/parse_to_json.py
@@ -0,0 +1,114 @@
+import json
+
+# считывание данных
+
+
+def parseJson(json, fileObject):
+    while True:
+        line = fileObject.readline().decode('utf-8')
+        if not line:
+            return json
+
+        line = line.rstrip('\r\n')
+
+        separator = line.find(":")
+
+        if separator == -1:
+            continue
+
+        key = line[:separator]
+        value = line[separator + 1:]
+
+        if key == "UID":
+            while True:
+                line = fileObject.readline().decode('utf-8')
+
+                line = line.rstrip('\r\n')
+
+                separator = line.find(":")
+
+                if separator != -1:
+                    key = line[:separator]
+
+                if key == "CREATED":
+                    json["mailto"] = value
+                    value = line[separator + 1:]
+                    break
+                else:
+                    value += line
+
+        if key == "BEGIN":
+            if value not in json:
+                json[value] = []
+            json[value].append(parseJson({}, fileObject))
+        elif key == "END":
+            return json
+        else:
+            json[key] = value
+
+# преобразование данных в нужный вид
+
+
+def redact(username, data):
+    for event in data:
+        # удаляем из event ненужное
+        del event["DTSTAMP"]
+        del event["CREATED"]
+        del event["DESCRIPTION"]
+        del event["SEQUENCE"]
+        del event["STATUS"]
+        del event["TRANSP"]
+        del event["LAST-MODIFIED"]
+        if "VALARM" in event:
+            del event["VALARM"]
+        # находим и изменяем файлы об организваторе и приглашенных
+        changeKey = event.keys()
+        myNewKey = None
+        for i in changeKey:
+            if i.find("ORGANIZER") != -1:
+                myNewKey = i
+        # переделываем поле организатор
+        if myNewKey is not None:
+            event['ORGANIZER'] = event.get(myNewKey)
+            separator = event["mailto"].find("mailto:")
+            if separator != -1:
+                event['ORGANIZER'] = event['ORGANIZER'][7:]
+            del event[myNewKey]
+        else:
+            del event["mailto"]
+            event["ORGANIZER"] = username
+            event["VISITORS"] = []
+
+        # переделываем поле приглашенных, если они есть
+        if "mailto" in event:
+            line = event["mailto"]
+            separator = event["mailto"].find("mailto:")
+            visitors = []
+
+            while separator != -1:
+                line = line[separator + 7:]  # взяли все что после mailto
+                sep = line.find("ATTENDEE;")  # считываем до att и короче посде mailto берем все email
+                if sep == -1:
+                    visitors.append(line)
+                    break
+                visitors.append(line[:sep])
+                separator = line.find("mailto:")
+            event["VISITORS"] = visitors
+            del event["mailto"]
+    return data
+
+
+def createjson (filename):
+    result = parseJson({}, filename)
+
+    myData = result.get('VCALENDAR')[0]  # извлекаем наш календарь
+    username = myData['X-WR-CALNAME']   # извлекаем имя владельца
+
+    # оставляем только поля event
+    myData = myData['VEVENT']
+    myData = redact(username, myData) # изменяем данные как необходимо
+
+    with open('./data/' + username + '.json', 'w') as outfile:  # создаем json файл
+        json.dump(myData, outfile, indent=4, ensure_ascii=False)
+
+    return username
